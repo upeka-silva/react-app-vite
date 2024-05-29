39a6134d8ee71908cf523ade35d6e293a482bc1d
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-29 19:25:27 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+     <>
+      <h1>customer data</h1>
+
+     <div className="card" >
+    <img src="https://images.squarespace-cdn.com/content/v1/5f13643bd87ba32558d653e7/c373a9ad-3795-41ff-ba12-9084d1242261/Arwen-Return-of-the-King-arwen-undomiel-34589904-950-1458.jpg" className="card-img-top" alt="..."/>
+    <div className="card-body">
+      <h5 className="card-title">Card title</h5>
+      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+        content.</p>
+      <a href="#" className="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+
+     </>
+
+
+  )
+}
+
+export default App
