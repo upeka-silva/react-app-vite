123bc279f98f1343831f1af69bbc4da73f8e2b9d
Pasindu Upeka <upekasilva2000@gmail.com>
2024-05-29 21:22:21 +0530

    3

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,8 @@ import './App.css'
              '         dolorum et ipsam ipsum natus sapiente! Autem facere quis sunt.',
          userProfile:'https://www.empireonline.com/movies/features/100-greatest-movie-characters/'
 
-     },{
+     },
+     {
          id:4,
          avatar:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzekyPrwf78iz8bwtVj_IyeS4-wN3hDlgpZw&s',
          customerName:'maker made',
@@ -52,7 +53,6 @@ import './App.css'
          userProfile:'https://www.wallflowerjournal.com/art-books-media/14-women-in-film-and-tv-that-make-us-love-being-women'
 
      }
-
 ]
 function App() {
 
@@ -61,15 +61,25 @@ function App() {
      <>
       <h1>customer data</h1>
 
-     <div className="card" >
-    <img src="https://images.squarespace-cdn.com/content/v1/5f13643bd87ba32558d653e7/c373a9ad-3795-41ff-ba12-9084d1242261/Arwen-Return-of-the-King-arwen-undomiel-34589904-950-1458.jpg" className="card-img-top" alt="..."/>
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-        content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
+         {
+
+             customers.map((customer)=> {
+
+                return (
+                    <div className="card" key={customer.id} onClick={()=>console.log(customer)}>
+                        <img src={customer.avatar} className="card-img-top" alt="..."/>
+                        <div className="card-body">
+                            <h5 className="card-title">{customer.customerName}</h5>
+                            <p className="card-text">{customer.description}</p>
+                            <a href={customer.userProfile} className="btn btn-primary" target='_blank' >Go somewhere</a>
+                        </div>
+                    </div>
+
+                )
+
+             })
+         }
+
 
      </>
 
